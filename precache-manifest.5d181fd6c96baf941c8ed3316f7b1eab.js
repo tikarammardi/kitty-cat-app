80f0101b3732cdb6ec5c7bd0c22b3301c1cd0561
Tikaram Mardi <tikarammardi007@gmail.com>
2019-04-28 16:40:39 +0530

    Updates

---
+++ precache-manifest.5d181fd6c96baf941c8ed3316f7b1eab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31285c323c65a25e8327d5a19779e4c9",
+    "url": "/kitty-cat-app/index.html"
+  },
+  {
+    "revision": "055094eb3fb8472c743a",
+    "url": "/kitty-cat-app/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/kitty-cat-app/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "055094eb3fb8472c743a",
+    "url": "/kitty-cat-app/static/js/main.807fd517.chunk.js"
+  },
+  {
+    "revision": "64f57c1cd3072549a431",
+    "url": "/kitty-cat-app/static/js/runtime~main.a78cbf4a.js"
+  },
+  {
+    "revision": "5d0e5c9beabb640bdf00249b1b448699",
+    "url": "/kitty-cat-app/static/media/logo.5d0e5c9b.png"
+  }
+]);
